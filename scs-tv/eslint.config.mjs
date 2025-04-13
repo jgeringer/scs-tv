a0255f404da9e02939343b7c6076c0590595fee6
Joseph Geringer <joegeringer@gmail.com>
2025-04-13 13:30:03 -0500

    removed img

--- scs-tv/eslint.config.mjs
+++ scs-tv/eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/ban-ts-comment": "off", // Globally disable the rule
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
